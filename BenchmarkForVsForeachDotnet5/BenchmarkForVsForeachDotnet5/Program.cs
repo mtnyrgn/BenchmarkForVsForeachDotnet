ff5fbc82ce39a7937b890456f4e012aa09db18cd
METIN YORGUN <myorgun@tpao.gov.tr>
2022-06-08 11:26:37 +0300

    project initialized

---
+++ BenchmarkForVsForeachDotnet5/BenchmarkForVsForeachDotnet5/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchmarkForVsForeachDotnet5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ForVsForEachBenchmark>();
+        }
+    }
+}
---
+++ BenchmarlForVsForeachDotnet6/BenchmarlForVsForeachDotnet6/ForVsForEachBenchmark.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarlForVsForeachDotnet6
+{
+    [MemoryDiagnoser]
+    public class ForVsForEachBenchmark
+    {
+        private static Random random = new Random();
+        private List<int> numberList;
+        int totalSum = 0;
+
+        [Params(10, 100, 1000, 10000)]
+        public int N;
+
+        public static List<int> RandomIntList(int length)
+        {
+            int min = 1;
+            int max = 10;
+
+            return Enumerable.Repeat(0, length).Select(i => random.Next(min, max)).ToList();
+        }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            numberList = RandomIntList(N);
+        }
+
+        [Benchmark(Baseline = true)]
+        public void CalculateTotalSumWithForEach()
+        {
+            foreach (int i in numberList)
+            {
+                totalSum += i;
+            }
+        }
+
+        [Benchmark]
+        public void CalculateTotalSumWithFor()
+        {
+            for (int i = 0; i < numberList.Count; i++)
+            {
+                totalSum += i;
+            }
+        }
+    }
+}
